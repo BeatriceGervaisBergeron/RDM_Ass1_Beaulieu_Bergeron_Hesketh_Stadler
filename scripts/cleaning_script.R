93479f1d7be8322cb31e6ec9d85ba18fcef685ff
Amelia Hesketh <avhesketh@gmail.com>
2020-11-25 14:37:32 -0800

    edited cleaning script & pushed clean data to appropriate folder

--- scripts/cleaning_script.R
+++ scripts/cleaning_script.R
@@ -1,6 +1,6 @@
 # LDP Data Cleaning Assignment
 
-pkgs <- c("lubridate","rgdal","tidyverse", "taxize", "myTAI")
+pkgs <- c("tidyverse", "taxize", "myTAI")
 lapply(pkgs, library,character.only = TRUE)
 
 ## Import Files ##
@@ -29,34 +29,47 @@ traits_tax <- traits %>%
 
 replace_missing_taxa <- function(df){
   clean_data <- data.frame()
+  levels_replaced <- c()
   for (row in 1:nrow(df)){
     x <- df[row,]
     col_names <- colnames(x)
+    levels_replaced_row <- c()
     #names of columns where taxonomic data are missing
     missing <- data.frame(numbers = which(is.na(x)), names = col_names[which(is.na(x))])
     # find taxonomic information based on lowest taxonomic level identified
-    tax_names <- tax_name(x$taxon_name, get = missing_names, db = "ncbi")
+    tax_names <- tax_name(x$taxon_name, get = missing$names, db = "ncbi")
+    tax_col_names <- colnames(tax_names)
     for (missing_col in 1:nrow(missing)){
       # for every column where a value was missing...
       for (filled_col in 1:length(tax_col_names)){
         # check the completed taxonomic information from the tax_name function
         if (missing$names[missing_col] == tax_col_names[filled_col]){
-          # and then fill in the missing data with the completed information
-          col_ref <- missing$numbers[missing_col]
-          x[1, col_ref] <- tax_names[1,filled_col]
+          # and then fill in the missing data with the completed information if it exists
+          if (is.na(tax_names[1, filled_col]) == 0){
+            col_ref <- missing$numbers[missing_col]
+            x[1, col_ref] <- tax_names[1,filled_col]
+            levels_replaced_row <- paste(levels_replaced_row, missing$names[missing_col])
+          }
         }
       }
     }
     # assemble all the clean data in a single dataframe
-  clean_data <- clean_data %>% 
-    rbind(x)
+    clean_data <- clean_data %>% 
+      rbind(x)
+    levels_replaced[row] <- paste(levels_replaced_row, collapse = "")
   }
-  # and return the clean data once all of the rows have been checked
-  return(clean_data)
+  # and amalgamate all of the data with the column indicating what has been replaced
+  clean_data <- clean_data %>% 
+    cbind(levels_replaced)
 }
 
+levels_replaced_row <- c()
 # now run the function on the taxonomic information for traits
 clean_traits <- replace_missing_taxa(traits_tax)
+
+clean_traits_df <- unique(as.data.frame(clean_traits))
+
+write_csv(clean_traits_df, "./clean_data/replaced_taxa.csv")
 
 # some taxa have multiple UID choices, so select the following as the tax_name function runs:
 # 2 for Culicoides, 2 for Forcipomyia, 2 for Chironomus, 2 for Polypedilum,
@@ -67,9 +80,11 @@ traits_complete <- traits %>%
   # take out incomplete taxonomic data
   select(-(4:17)) %>% 
   # and join with the completed taxonomic data
-  full_join(clean_traits)
+  full_join(clean_traits) %>% 
+  # the tax_name function seems to replace suborder with infraorder sometimes, 
+  # so retain only the first instance of replacement for each unique taxon
+  distinct(bwg_name, .keep_all = TRUE)
 
 # taxonomic data are now fully integrated into dataframe
 
 write_csv(traits_complete,"./clean_data/traits_complete.csv")
-
